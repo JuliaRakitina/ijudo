8a54ba45479bfaca93641189b1253fb13652a16f
Julia Rakitina <YuliyaR@bak2001.bakcell.com>
2014-06-11 15:17:09 +0500

    splash screen

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,8 +1,12 @@
 /**
  * Created by YuliyaR on 6/9/14.
  */
+
+
 $(document).bind( "mobileinit", function() {
     // Make your jQuery Mobile framework configuration changes here!
 
     $.mobile.allowCrossDomainPages = true;
+    // Wait for Cordova to load
+//
 });
