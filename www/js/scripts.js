3f9dbcfa6eada4e29d836ea6bb128584f37d10ac
Julia Rakitina <YuliyaR@bak2001.bakcell.com>
2014-06-23 00:21:09 +0500

    commit

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -8,5 +8,10 @@ $(document).bind( "mobileinit", function() {
 
     $.mobile.allowCrossDomainPages = true;
     // Wait for Cordova to load
+
+    $.mobile.loader.prototype.options.text = "loading";
+    $.mobile.loader.prototype.options.textVisible = false;
+    $.mobile.loader.prototype.options.theme = "a";
+    $.mobile.loader.prototype.options.html = "";
 //
 });
